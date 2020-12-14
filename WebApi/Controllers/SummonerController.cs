828e5cbc76759b8673087a2710a6b9dac5235635
michal-sob <michalkacpernowak@gmail.com>
2020-12-14 11:38:09 +0100

    Controllers moved to WebApi folder

---
+++ WebApi/Controllers/SummonerController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MatchStatistic.WebApi.Models;
+using MatchStatistic.WebApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchStatistic.WebApi.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class SummonerController : ControllerBase
+	{
+
+		private readonly SummonerService _summonerService;
+
+		public SummonerController(SummonerService summonerService)
+		{
+			_summonerService = summonerService;
+		}
+
+		// GET: api/<SummonerController>
+		[HttpGet]
+		public async Task<ActionResult<SummonerDTO>> Get()
+		{
+			//Task<ActionResult<SummonerDTO>>
+			var results = await _summonerService.GetSummByName("eun1", "Paczatek");
+
+			return results;
+		}
+
+		// GET api/<SummonerController>/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+	}
+}
