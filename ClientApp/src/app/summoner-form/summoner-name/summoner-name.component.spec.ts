842b27da47f0f7e561ed2f227f0fab42b62ce9d8
lukasz-mielczarek <lukasz.mielczarek1989@gmail.com>
2020-12-15 09:20:45 +0100

    Add summoner-viev

---
+++ ClientApp/src/app/summoner-form/summoner-name/summoner-name.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { SummonerNameComponent } from './summoner-name.component';
+
+let component: SummonerNameComponent;
+let fixture: ComponentFixture<SummonerNameComponent>;
+
+describe('summonerName component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ SummonerNameComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(SummonerNameComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
---
+++ ClientApp/src/app/summoner-form/summoner-name/summoner-name.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-summoner-name',
+    templateUrl: './summoner-name.component.html',
+    styleUrls: ['./summoner-name.component.scss']
+})
+/** summonerName component*/
+export class SummonerNameComponent {
+    /** summonerName ctor */
+    constructor() {
+
+    }
+}
