087e54a089a72ac8527a326550e850299a7d4472
lukasz-mielczarek <lukasz.mielczarek1989@gmail.com>
2021-01-28 18:35:27 +0100

    add 1 matchhistory

--- ClientApp/src/app/account/account.component.ts
+++ ClientApp/src/app/account/account.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute } from "@angular/router";
 import { LeagueEntry } from '../models/leagueEntry.model';
 import { UserSite } from '../models/userSite.model';
 import { GetServer } from '../util/Servers.enum';
+import MatchData from '../../assets/_files/matchhistory1.json';
 
 @Component({
   selector: 'app-account',
@@ -14,12 +15,39 @@ export class AccountComponent  {
 
   private server;
   private summonerName;
+  private matchData;
+  private matchDataTime; 
+  private matchDataLong;
+  private correctSummoner;
+
+  
 
   public userData: UserSite;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, private route: ActivatedRoute) {
     this.server = this.route.snapshot.paramMap.get("loc");
+
     this.summonerName = this.route.snapshot.paramMap.get("summonerName");
+
+    this.matchData = MatchData;
+
+    this.correctSummoner = this.matchData.participants[getIndexCurrentSummonerFromAllSummoners(this.summonerName, this.matchData)];
+
+    console.log(this.correctSummoner);
+    // Match duration in seconds.
+    
+    var date = new Date(this.matchData.gameDuration);
+    var hours = date.getHours();
+    var minutes = "0" + date.getMinutes();
+    var seconds = "0" + date.getSeconds();
+    this.matchDataLong = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+    // Designates the timestamp when champion select ended and the loading screen appeared, NOT when the game timer was at 0:00.
+
+    const d = new Date(this.matchData.gameCreation);
+    this.matchDataTime = d.getHours() + ":" + d.getMinutes() + ", " + d.toDateString();
+
+    
 
     http.get<UserSite>(`${baseUrl}api/Summoner/summonerSiteDTO/by-name/${GetServer(this.server)}/${this.summonerName}`).subscribe(result => {
       this.userData = result,
@@ -27,6 +55,18 @@ export class AccountComponent  {
         err => console.log(err),
         () => console.log("Request Done")
     })
+    function getIndexCurrentSummonerFromAllSummoners(summonerName, matchData) {
+      let index = 0;
+
+      for (let summoner of matchData.participantIdentities) {
+        
+        if (summoner.player.summonerName == summonerName) {
+          
+          return index;
+        }
+        index += 1;
+      }
+    }
 
     //http.get(baseUrl + "api/" + "Summoner/" + this.server + '/' + this.summonerName).subscribe(result => {
     //  this.summoner = result[0],
