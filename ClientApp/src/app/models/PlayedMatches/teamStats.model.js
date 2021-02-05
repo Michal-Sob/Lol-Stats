7c4839b5ca88fde9339a150240438cb90d2df332
Michał <michalkacpernowak@gmail.com>
2021-02-05 14:02:20 +0100

    PLayed Matches TS TeamStats Model

---
+++ ClientApp/src/app/models/PlayedMatches/teamStats.model.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TeamStats = void 0;
+var TeamStats = /** @class */ (function () {
+    function TeamStats() {
+    }
+    return TeamStats;
+}());
+exports.TeamStats = TeamStats;
+//# sourceMappingURL=teamStats.model.js.map
